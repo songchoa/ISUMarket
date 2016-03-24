91109052a7176ae2b2a4c795838628ba3ea9d645
Chao <songchao.w.h@gmail.com>
2016-03-23 18:36:06 -0700

    textbook page - navigation bar

---
+++ php/textbook.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ISU Market - TEXTBOOK</title>
+    <link rel="stylesheet" href="../css/mainStyle.css" type="text/css"> 
+
+</head>
+
+<body>
+    <div id="mainContainer">
+        <div id="header">
+            <div id="headerWrapper">
+                <div id="imageHolder">
+                    <img src="../images/isumarketlogo.png" width="180px" height="60px">
+                </div>
+
+                <div id="menuHolder">
+                    <div id="textHolder">
+                        <ul id="navbar">
+                            <li><a href="../index.php">Home</a></li>
+                            <li><a href="php/meanPlan.php">Meal Plan</a></li>
+                            <li><a href="php/textbook.php" class="active">Textbook</a></li>
+                            <li><a href="php/tech.php">Technology</a></li>
+                            <li><a href="php/others.php">Others</a></li>
+                            <li style="float: right;"><a href="login.php">Login or Signup</a></li>
+                        </ul>
+                    </div>
+                </div> <!-- menuHolder -->
+            </div> <!-- headerWrapper -->
+        </div>  <!-- header -->
+
+        <div id="mainContent">
+            <div id="contentHolder">
+                <div id="contentHeader">
+                    <h1 class="subheader">TEXTBOOK</h1>
+                </div>
+                <div>
+                    <table>
+                        <tr class="book 1">
+                            <th>
+                                <img src="../images/isumarketlogo.png" width="180px" height="120px">
+                            </th>
+
+                            <th>
+                                <a class="title" href="asdflkjlk">Caculus II</a>
+                                <p>A very good book but very hard to learn.</p>
+                              
+                            </th>
+                        </tr>
+
+                        <tr class="book 2">
+                            <th>
+                                <img src="../images/isumarketlogo.png" width="180px" height="120px">
+                            </th>
+
+                            <th>
+                                <a class="title" href="asdflkjlk">Caculus II</a>
+                                <p>A very good book but very hard to learn.</p>
+                                
+                            </th>
+                        </tr>
+
+
+                    </table>
+                </div>
+                          
+
+            </div> <!-- contentHolder -->   
+        </div> <!-- mainContent -->
+
+    </div> <!-- mainContainer -->
+</body>
+
+</html>
