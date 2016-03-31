1a8d9bb0458771b16c8b23cedb5f232e0abe49cc
songchoa <chaos@iastate.edu>
2016-03-31 11:52:22 -0500

    upload picture and information like title, price, intro, etc

--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,7 @@
 <?php
-$target_dir = "/uploads";
+session_start();
+
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -36,9 +38,35 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+       // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        // echo '<script>';
+        // echo 'alert("good")';
+        // echo '</script>';
+
+        include('connectDB.php');
+
+        $title = $_POST["title"];
+        $intro = $_POST["intro"];
+        $price = $_POST["price"];
+        $picture = $_FILES['fileToUpload']['name'];
+        $contact = $_POST["contact"];
+        $table = $_SESSION['recent'];
+
+        $sql = "INSERT INTO " . $table . "(title,intro,picture,price,contact) VALUES ( '$title','$intro','$picture','$price','$contact')";
+    
+        if ($dbconn->query($sql) === TRUE) {
+            $dbconn->close();
+          header('Location: http://localhost/www/ISUMarket/php/' .$table.'.php');
+        } else {
+            echo "Error: " . $dbconn->connect_error;
+            $dbconn->close();
+        }
+
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+
+
+
 ?>
