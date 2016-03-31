d1845e4a73dc279c529125b276fa82efea40d974
songchoa <chaos@iastate.edu>
2016-03-31 11:53:29 -0500

    keep this file for later use

---
+++ php/storeItem.php
@@ -0,0 +1,9 @@
+<?php  
+	// echo $_POST["title"];
+	// echo $_POST["price"];
+	// echo $_POST["contact"];
+	// echo $_POST["intro"];
+	// echo $_POST["uploadfile"];
+
+	echo $_POST["textfield"];
+?>
